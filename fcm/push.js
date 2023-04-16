dd52e991cd5bb10220aaa17fcfdbe2f76d6a14ee
segoonkang <hidlng@naver.com>
2023-04-16 22:27:03 +0900

    sdfsdf

--- fcm/push.js
+++ fcm/push.js
@@ -10,12 +10,6 @@ module.exports = {
     },
 
     sendFcm : async function( dkey, name , option) {
-		//dkey = "f16pl1IwSt6M_7eqmh1YG4:APA91bHonj654KR6yNfKC-580sl4NjD38iw1vUTtMA9ymWwWeqB2eNG5uBLYBBWtnJLSbc36hC8cshGrtZJ6hxSrP_m3bh0QLWvVfowHRH-WDSPsssgARykNL2_79itOks1rzyJwOBni";
-		//name = "CIAT";
-        // var data_key = String( push_key );
-        // var site_id = String( site_id );
-        // var site_type = String( site_type );
-        // var site_name = String( site_name );
         var payload = {
             data : {
                 "title" : "TANGO 스치",
@@ -27,14 +21,16 @@ module.exports = {
         console.log( dkey );
         console.log( payload );
 
-        admin.messaging().sendToDevice( dkey, payload  ).then(function(response){
-            console.log('success');
-            return 200;            
-        }).catch(function(error){
-			console.log('error');
-            console.log(error);
-            return 500;
-        });
+        if( dkey != undefined ) {
+            admin.messaging().sendToDevice( dkey, payload  ).then(function(response){
+                console.log('success');
+                return 200;            
+            }).catch(function(error){
+                console.log('error');
+                console.log(error);
+                return 500;
+            });
+        }
     }
 
 }
