984d30867d205311fbdf9892beae1785bfc1e74a
hidlng <hidlng@naver.com>
2024-03-14 22:57:07 +0900

    121212

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var http = require('http');
+
+var pool = require('./db/config');
+
+const schedule = require('node-schedule');
+const mongoose = require('mongoose');
 const cron = require('node-cron');
 const dotenv = require('dotenv');
 var redis = require( 'redis' );
@@ -63,8 +68,97 @@ app.use('/users', usersRouter);
 app.use('/test_api', test_api);
 app.use('/f_api', f_api);
 
+//mongodb
+mongoose
+.connect(`${process.env.MONGODB_URI}`,{useNewUrlParser : true, useUnifiedTopology: true})
+.then(() => console.log('Mongodb connected!'))
+.catch(e => console.error(e));
+
 //push init
 fcm_common.initFcm();
+
+//7.시 알람
+schedule.scheduleJob({hour: 7, minute: 0}, async function(){
+
+  const mentArr = [
+    '길 위에서의 동료와 함께 달려보세요. 탱고',
+    '오늘도 스치하기 좋은 날입니다. 탱고',
+    '다함께 말고, 우리꺼! 트럭커 온리. 탱고',
+    '운전석에서 100명의 동료와 만나는 방법, 탱고',
+    '무사고 매출 대박 기원! 오늘도 안전운전하세요',
+
+  ];
+  const randomMent = mentArr[Math.floor(Math.random() * mentArr.length)];
+  var checkSql = ` SELECT a.pid FROM tanggodb.userinfo`;
+	var chckData = await executeQuery(pool2, checkSql, []);
+	if( chckData.length > 0 ) {
+		chckData.forEach(function(item) {
+			fcm_common.sendFcm(item.pid, randomMent, "1");
+		});
+  }
+});
+
+//10시 알람
+schedule.scheduleJob({hour: 10, minute: 0}, async function(){
+  const mentArr = [
+    '길 위에서의 동료와 함께 달려보세요. 탱고',
+    '오늘도 스치하기 좋은 날입니다. 탱고',
+    '다함께 말고, 우리꺼! 트럭커 온리. 탱고',
+    '운전석에서 100명의 동료와 만나는 방법, 탱고',
+    '무사고 매출 대박 기원! 오늘도 안전운전하세요',
+
+  ];
+  const randomMent = mentArr[Math.floor(Math.random() * mentArr.length)];
+  var checkSql = ` SELECT a.pid FROM tanggodb.userinfo`;
+	var chckData = await executeQuery(pool2, checkSql, []);
+	if( chckData.length > 0 ) {
+		chckData.forEach(function(item) {
+			fcm_common.sendFcm(item.pid, randomMent, "1");
+		});
+  }
+});
+
+//14시 알람
+schedule.scheduleJob({hour: 14, minute: 0}, async function(){
+  const mentArr = [
+    '길 위에서의 동료와 함께 달려보세요. 탱고',
+    '오늘도 스치하기 좋은 날입니다. 탱고',
+    '다함께 말고, 우리꺼! 트럭커 온리. 탱고',
+    '운전석에서 100명의 동료와 만나는 방법, 탱고',
+    '무사고 매출 대박 기원! 오늘도 안전운전하세요',
+
+  ];
+  const randomMent = mentArr[Math.floor(Math.random() * mentArr.length)];
+  var checkSql = ` SELECT a.pid FROM tanggodb.userinfo`;
+	var chckData = await executeQuery(pool2, checkSql, []);
+	if( chckData.length > 0 ) {
+		chckData.forEach(function(item) {
+			fcm_common.sendFcm(item.pid, randomMent, "1");
+		});
+  }
+});
+
+//18시 알람
+schedule.scheduleJob({hour: 18, minute: 0}, async function(){
+  const mentArr = [
+    '길 위에서의 동료와 함께 달려보세요. 탱고',
+    '오늘도 스치하기 좋은 날입니다. 탱고',
+    '다함께 말고, 우리꺼! 트럭커 온리. 탱고',
+    '운전석에서 100명의 동료와 만나는 방법, 탱고',
+    '무사고 매출 대박 기원! 오늘도 안전운전하세요',
+
+  ];
+  const randomMent = mentArr[Math.floor(Math.random() * mentArr.length)];
+  var checkSql = ` SELECT a.pid FROM tanggodb.userinfo`;
+	var chckData = await executeQuery(pool2, checkSql, []);
+	if( chckData.length > 0 ) {
+		chckData.forEach(function(item) {
+			fcm_common.sendFcm(item.pid, randomMent, "1");
+		});
+  }
+});
+
+
 
 var server = http.createServer(app).listen(app.get('port'), '0.0.0.0');
 webSocketServer(server);
